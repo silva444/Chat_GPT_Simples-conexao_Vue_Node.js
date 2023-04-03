589f1d62fa39d185b4fd729eafd65091a47d1694
vinicius3382 <69991670+vinicius3382@users.noreply.github.com>
2023-04-02 22:25:52 -0300

    atualização

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -62,22 +62,32 @@ export default {
       //   .catch((err) => {
       //     console.log(err);
       //   });
+
       const { Configuration, OpenAIApi } = require("openai");
 
-      const configuration = new Configuration({
-        apiKey: "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",
-      });
-      const openai = new OpenAIApi(configuration);
+const configuration = new Configuration({
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+try {
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: "Hello world",
+  });
+  console.log(completion.data.choices[0].text);
+} catch (error) {
+  if (error.response) {
+    console.log(error.response.status);
+    console.log(error.response.data);
+  } else {
+    console.log(error.message);
+  }
+}
+//console.log(completion.data.choices[0].text);
 
 
-
-      const completion = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: `${this.input}`,
-      });
-      console.log(completion.data.choices[0].text);
-      this.texts = completion.data.choices[0].text;
-
+      
     }
   }
 
